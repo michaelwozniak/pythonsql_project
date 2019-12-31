9e6ceb761fd05cf2eebef5e07f89db2f5f03881d
Michał Woźniak <mj.wozniak9@student.uw.edu.pl>
2019-12-31 15:10:26 +0100

    code updated + db updated

--- application.py
+++ application.py
@@ -10,24 +10,25 @@ import time
 import string
 import random
 import shutil
-from model import Model
 import tensorflow as tf
 from keras.applications.inception_resnet_v2 import InceptionResNetV2
+from model import Model
 
 # deep learning init for multithreading in flask
 global graph
 graph = tf.get_default_graph()
 model = InceptionResNetV2(weights='imagenet', include_top=False, classes=1000) 
 
+# flask app init
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/')
 def index():
-    c = sqlite3.connect('databases/coreApp.db')
-    conn = c.cursor()
-    cursor = conn.execute("SELECT * from HOME")
-    return render_template('index.html',texts=cursor)
+    db = sqlite3.connect('databases/coreApp.db')
+    cursor = db.cursor()
+    query = cursor.execute("SELECT * from HOME")
+    return render_template('index.html',texts=query)
 
 @app.route('/features')
 def features():
@@ -39,45 +40,47 @@ def features_more():
 
 @app.route('/contact')
 def contact():
-    c = sqlite3.connect('databases/coreApp.db')
-    conn = c.cursor()
-    cursor = conn.execute("SELECT * from CONTACT")
-    return render_template('contact.html', texts=cursor)
+    db = sqlite3.connect('databases/coreApp.db')
+    cursor = db.cursor()
+    query = cursor.execute("SELECT * from CONTACT")
+    return render_template('contact.html', texts=query)
 
 class RegisterForm(Form):
     username = StringField('Username', [validators.Length(min=5, max=25)])
     name = StringField('Name', [validators.Length(min=2, max=50)])
     surname = StringField('Surname', [validators.Length(min=2, max=50)])
     email = StringField('Email', [validators.Length(min=6, max=50)])
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Wrong password')
-    ])
+    password = PasswordField('Password', [validators.DataRequired(),validators.EqualTo('confirm', message='Wrong password')])
     confirm = PasswordField('Confirm password')
 
 @app.route("/signup", methods = ["GET","POST"])
 def signup():
     form = RegisterForm(request.form)
+
     if request.method == "POST" and form.validate() == True:
         username = form.username.data
         name = form.name.data
         surname = form.surname.data
         email = form.email.data
+
         if "@" not in email:
             flash("Wrong email adress, please correct it!","danger")
             return render_template("signup.html", form = form)
+
         password = sha256_crypt.encrypt(str(form.password.data))
-        c = sqlite3.connect('databases/coreApp.db')
-        conn = c.cursor()
-        conn.execute("SELECT * FROM users WHERE email = ?", (email,))
-        tmp = conn.fetchall()
+        db = sqlite3.connect('databases/coreApp.db')
+        cursor= db.cursor()
+        cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
+        tmp = cursor.fetchall()
+
         if (len(tmp)!=0):
             flash("Account with this email already exist! Please sign in!","danger")
             return render_template("signup.html", form = form)
         else:
-            conn.execute('''INSERT INTO users(name, surname, email, username, password) VALUES(?,?,?,?,?)''', (name, surname, email, username, password))
-            c.commit()
-            c.close()
+            cursor.execute('''INSERT INTO users(name, surname, email,\
+                 username, password) VALUES(?,?,?,?,?)''', (name, surname, email, username, password))
+            db.commit()
+            db.close()
             flash('Signed up! Now, feel free to sign in :-)', 'success')
             return redirect(url_for('signup'))
     return render_template("signup.html", form = form)
@@ -90,20 +93,24 @@ class LoginForm(Form):
 @app.route("/signin", methods = ["GET","POST"])
 def signin():
     form = LoginForm(request.form)
+
     if request.method == 'POST':
         email = form.email.data
         password_tmp = form.password.data
-        c = sqlite3.connect('databases/coreApp.db')
-        conn = c.cursor()
-        conn.execute("SELECT * FROM users WHERE email = ?", (email,))
-        tmp = conn.fetchall()
+
+        db = sqlite3.connect('databases/coreApp.db')
+        cursor= db.cursor()
+        cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
+        tmp = cursor.fetchall()
+
         if (len(tmp) != 0):
-            conn.execute("SELECT * FROM users WHERE email = ?", (email,))
-            conn.row_factory = sqlite3.Row 
-            rows = conn.fetchall()
+            cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
+            cursor.row_factory = sqlite3.Row 
+            rows = cursor.fetchall()
             data = dict(rows[0])
-            conn.close()
+            db.close()
             password = data['password']
+
             if sha256_crypt.verify(password_tmp, password):
                 session['logged_in'] = True
                 session['email'] = email
@@ -112,7 +119,7 @@ def signin():
                 flash('You are logged in', 'success')
                 return redirect(url_for('index'))
             else:
-                error = 'Invalid login, try again!'
+                error = 'Something goes wrong, try again!'
                 return render_template('signin.html', error=error,form=form)
         else:
             error = 'Email not found'
@@ -139,17 +146,17 @@ def logout():
 @app.route("/profile")
 @logged_in_checker
 def profile():
-    c = sqlite3.connect('databases/coreApp.db')
-    conn = c.cursor()
+    db = sqlite3.connect('databases/coreApp.db')
+    cursor = db.cursor()
     tmp = f"CREATE VIEW profile_view AS SELECT name, surname, username, register_date, company_name, field_of_research, \
         phone_number,job_position FROM users as A LEFT JOIN users_additional_informations as B on B.ID = A.ID WHERE A.ID = {session.get('ID')}"
-    conn.execute("DROP VIEW IF EXISTS profile_view")
-    conn.execute(tmp)
-    conn.execute("SELECT * FROM profile_view")
-    conn.row_factory = sqlite3.Row 
-    rows = conn.fetchall()
+    cursor.execute("DROP VIEW IF EXISTS profile_view")
+    cursor.execute(tmp)
+    cursor.execute("SELECT * FROM profile_view")
+    cursor.row_factory = sqlite3.Row 
+    rows = cursor.fetchall()
     data = dict(rows[0])
-    conn.close()
+    db.close()
     return render_template("profile.html", text=data)
 
 class ProfileForm(Form):
@@ -164,6 +171,7 @@ class ProfileForm(Form):
 @logged_in_checker
 def profile_edit():
     form = ProfileForm(request.form)
+
     if request.method == 'POST' and form.validate() == True:
         name = form.name.data
         surname = form.surname.data
@@ -171,26 +179,29 @@ def profile_edit():
         field_of_research = form.field_of_research.data
         phone_number = form.phone_number.data
         job_position = form.job_position.data
-        c = sqlite3.connect('databases/coreApp.db')
-        conn = c.cursor()
-        conn.execute("UPDATE users SET name = ?, surname = ? WHERE ID = ?", (name,surname,session.get("ID"),))
-        c.commit()
-        conn = c.cursor()
-        conn.execute("SELECT * FROM users_additional_informations WHERE ID = ?", (session.get("ID"),))
-        tmp = conn.fetchall()
+
+        db = sqlite3.connect('databases/coreApp.db')
+        cursor = db.cursor()
+        cursor.execute("UPDATE users SET name = ?, surname = ? WHERE ID = ?", (name,surname,session.get("ID"),))
+        db.commit()
+
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM users_additional_informations WHERE ID = ?", (session.get("ID"),))
+        tmp = cursor.fetchall()
+
         if (len(tmp) != 0):
-            conn = c.cursor()
-            conn.execute("UPDATE users_additional_informations SET company_name = ?, field_of_research = ?,\
+            cursor = db.cursor()
+            cursor.execute("UPDATE users_additional_informations SET company_name = ?, field_of_research = ?,\
                  phone_number = ?, job_position = ? WHERE ID = ?", (company_name,field_of_research,phone_number,job_position, session.get("ID"),))
-            c.commit()
-            conn.close()
+            db.commit()
+            db.close()
             return redirect(url_for('profile'))
         else:
-            conn = c.cursor()
-            conn.execute("INSERT INTO users_additional_informations (ID, company_name, field_of_research, \
+            cursor= db.cursor()
+            cursor.execute("INSERT INTO users_additional_informations (ID, company_name, field_of_research, \
                 phone_number , job_position) VALUES (?,?,?,?,?)", (session.get("ID"),company_name,field_of_research,phone_number,job_position,))
-            c.commit()
-            conn.close()
+            db.commit()
+            db.close()
             return redirect(url_for('profile'))
 
     return render_template('profile_edit.html', form = form)
@@ -211,20 +222,23 @@ def creator():
         title = form.title.data
         number_of_clusters = str(form.number_of_clusters.data)
         comments = form.comments.data
-        c = sqlite3.connect('databases/coreApp.db')
-        conn = c.cursor()
+
+        db = sqlite3.connect('databases/coreApp.db')
+        cursor = db.cursor()
         hash_gen = id_generator(40)
-        conn.execute("INSERT INTO projects(user_ID,hash) VALUES (?,?)", (session.get("ID"),hash_gen))
-        c.commit()
-        conn.execute("select * from projects WHERE user_ID = ? ORDER BY ID DESC LIMIT 1;", (session.get("ID"),))
-        conn.row_factory = sqlite3.Row 
-        rows = conn.fetchall()
+        cursor.execute("INSERT INTO projects(user_ID,hash) VALUES (?,?)", (session.get("ID"),hash_gen))
+        db.commit()
+
+        cursor.execute("select * from projects WHERE user_ID = ? ORDER BY ID DESC LIMIT 1;", (session.get("ID"),))
+        cursor.row_factory = sqlite3.Row 
+        rows = cursor.fetchall()
         data = dict(rows[0])
         project_id = data["ID"]
-        conn = c.cursor()
-        conn.execute("INSERT INTO projects_settings(ID, title, number_of_clusters, comments) VALUES (?,?,?,?)", (project_id,title,number_of_clusters,comments))
-        c.commit()
-        c.close()
+
+        cursor= db.cursor()
+        cursor.execute("INSERT INTO projects_settings(ID, title, number_of_clusters, comments) VALUES (?,?,?,?)", (project_id,title,number_of_clusters,comments))
+        db.commit()
+        db.close()
         return redirect(url_for('creator_img'))
     return render_template('creator.html',form = form)
 
@@ -246,38 +260,43 @@ def creator_img():
             foo_destination = "/".join([target,foo_name])
             os.rename(destination,foo_destination)
             controller = controller + 1
-            c = sqlite3.connect('databases/coreApp.db')
-            conn = c.cursor()
-            conn.execute("select * from projects WHERE user_ID = ? ORDER BY ID DESC LIMIT 1;", (session.get("ID"),))
-            conn.row_factory = sqlite3.Row 
-            rows = conn.fetchall()
+
+            db = sqlite3.connect('databases/coreApp.db')
+            cursor= db.cursor()
+            cursor.execute("select * from projects WHERE user_ID = ? ORDER BY ID DESC LIMIT 1;", (session.get("ID"),))
+            cursor.row_factory = sqlite3.Row 
+            rows = cursor.fetchall()
             data = dict(rows[0])
             project_id = data["ID"]
-            c = sqlite3.connect('databases/coreApp.db')
-            conn = c.cursor()
-            conn.execute("INSERT INTO images(img_name, project_ID) VALUES (?,?)", (foo_destination,project_id,))
-            c.commit()
-            c.close()
+
+            db = sqlite3.connect('databases/coreApp.db')
+            cursor= db.cursor()
+            cursor.execute("INSERT INTO images(img_name, project_ID) VALUES (?,?)", (foo_destination,project_id,))
+            db.commit()
+            db.close()
 
     if controller != 0:
         time.sleep(2)
+
         task = f"create view images_to_model as select img_name from images WHERE project_id = {project_id}"
-        c = sqlite3.connect('databases/coreApp.db')
-        conn = c.cursor()
-        conn.execute("DROP VIEW IF EXISTS images_to_model")
-        conn.execute(task)
-        conn.execute("select * from images_to_model")
-        rows = conn.fetchall()
+        db = sqlite3.connect('databases/coreApp.db')
+        cursor = db.cursor()
+        cursor.execute("DROP VIEW IF EXISTS images_to_model")
+        cursor.execute(task)
+        cursor.execute("select * from images_to_model")
+
+        rows = cursor.fetchall()
         files_list = list()
         for i in rows:
             files_list.append(i[0])
 
-        conn = c.cursor()
-        conn.execute("SELECT number_of_clusters FROM projects_settings WHERE ID = ?", (project_id,))
-        conn.row_factory = sqlite3.Row 
-        rows = conn.fetchall()
+        cursor = db.cursor()
+        cursor.execute("SELECT number_of_clusters FROM projects_settings WHERE ID = ?", (project_id,))
+        cursor.row_factory = sqlite3.Row 
+        rows = cursor.fetchall()
         data = dict(rows[0])
         number_of_clusters = int(data["number_of_clusters"])
+
         try:
             with graph.as_default():
                 cnn_birch_model = Model(files_list,number_of_clusters,model)
@@ -286,21 +305,22 @@ def creator_img():
                 cnn_birch_model.pca_plot()
                 cnn_birch_model.sillhouette_plot()
                 labels = cnn_birch_model.birch_model_and_plot()
-            conn = c.cursor()
-            conn.execute("SELECT ID FROM images WHERE project_id = ?", (project_id,))
-            rows = conn.fetchall()
+            cursor= db.cursor()
+            cursor.execute("SELECT ID FROM images WHERE project_id = ?", (project_id,))
+            rows = cursor.fetchall()
             img_tmp = list()
             for i in rows:
                 img_tmp.append(i[0])
 
             for i in range(len(labels)):
-                conn = c.cursor()
-                conn.execute("INSERT INTO images_clusters(ID, clusters) VALUES (?,?)", (img_tmp[i],int(labels[i]),))
-                c.commit()
+                cursor= db.cursor()
+                cursor.execute("INSERT INTO images_clusters(ID, clusters) VALUES (?,?)", (img_tmp[i],int(labels[i]),))
+                db.commit()
 
-            conn = c.cursor()
-            conn.execute("INSERT INTO extra_plots(plot1_name, plot2_name, plot3_name, project_ID) VALUES (?,?, ?,?)", (cnn_birch_model.sillhouette_name,cnn_birch_model.pca_name,cnn_birch_model.birch_name,project_id))
-            c.commit()
+            cursor= db.cursor()
+            cursor.execute("INSERT INTO extra_plots(plot1_name, plot2_name, plot3_name, project_ID) VALUES \
+                (?,?, ?,?)", (cnn_birch_model.sillhouette_name,cnn_birch_model.pca_name,cnn_birch_model.birch_name,project_id))
+            db.commit()
 
             flash("Computations completed!","success")
             time.sleep(2)
@@ -314,39 +334,42 @@ def creator_img():
 @app.route("/release")
 @logged_in_checker
 def release():
-    c = sqlite3.connect('databases/coreApp.db')
-    conn = c.cursor()
+    db = sqlite3.connect('databases/coreApp.db')
+    cursor = db.cursor()
     command = f"CREATE VIEW dashboard as SELECT title, number_of_clusters, create_date, COUNT(c.ID) as number_of_images,\
          hash FROM projects_settings AS a LEFT JOIN projects as b ON a.ID = b.ID LEFT JOIN images as c ON a.ID = c.project_id\
               LEFT JOIN extra_plots as d ON a.ID = d.project_ID WHERE user_id = {session.get('ID')} AND d.project_ID IS\
                    NOT NULL GROUP BY c.project_id ORDER BY create_date"    
-    conn.execute("DROP VIEW IF EXISTS dashboard")
-    conn.execute(command)
-    conn.execute("SELECT * from dashboard")
-    conn.row_factory = sqlite3.Row 
-    rows = conn.fetchall()
+    cursor.execute("DROP VIEW IF EXISTS dashboard")
+
+    cursor.execute(command)
+
+    cursor.execute("SELECT * from dashboard")
+    cursor.row_factory = sqlite3.Row 
+    rows = cursor.fetchall()
     data = [dict(i) for i in rows]
+
     if len(data) > 0:
         return render_template('release.html', data=data)
     else:
         msg = 'Empty! Create some projects!'
         return render_template('release.html', msg=msg)
-    conn.close()
+    db.close()
 
 @app.route('/case/<string:hash_id>/')
 @logged_in_checker
 def case(hash_id):
-    c = sqlite3.connect('databases/coreApp.db')
-    conn = c.cursor()
-    conn.execute("SELECT name, surname, email, company_name, field_of_research, phone_number, job_position,\
+    db = sqlite3.connect('databases/coreApp.db')
+    cursor= db.cursor()
+    cursor.execute("SELECT name, surname, email, company_name, field_of_research, phone_number, job_position,\
          title, number_of_clusters,comments,create_date, plot1_name, plot2_name, plot3_name, clusters , \
              count(name) as number_of_images FROM users LEFT JOIN users_additional_informations ON \
                  users.ID = users_additional_informations.ID LEFT JOIN projects ON users.ID = projects.user_ID LEFT JOIN projects_settings\
                       ON  projects_settings.ID = projects.ID LEFT JOIN extra_plots ON extra_plots.project_ID = projects.ID \
                           LEFT JOIN images ON projects_settings.ID = images.project_ID LEFT JOIN images_clusters ON \
                               images.ID = images_clusters.ID  WHERE hash = ? GROUP BY plot1_name,clusters", [hash_id])
-    conn.row_factory = sqlite3.Row 
-    rows = conn.fetchall()
+    cursor.row_factory = sqlite3.Row 
+    rows = cursor.fetchall()
     data = [dict(i) for i in rows]
     _ , data[0]["plot1_name"] = data[0]["plot1_name"].split("images/output/")
     _ , data[0]["plot2_name"] = data[0]["plot2_name"].split("images/output/")
@@ -359,11 +382,11 @@ def case(hash_id):
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-        conn = c.cursor()
-        conn.execute("SELECT img_name, clusters FROM images LEFT JOIN images_clusters ON images.ID = images_clusters.ID \
+        cursor= db.cursor()
+        cursor.execute("SELECT img_name, clusters FROM images LEFT JOIN images_clusters ON images.ID = images_clusters.ID \
             LEFT JOIN projects ON images.project_ID = projects.ID WHERE hash = ?",[hash_id])
-        conn.row_factory = sqlite3.Row 
-        rows = conn.fetchall()
+        cursor.row_factory = sqlite3.Row 
+        rows = cursor.fetchall()
         df = [dict(i) for i in rows]
         df = pd.DataFrame(df)
 
@@ -386,7 +409,7 @@ def case(hash_id):
         shutil.rmtree(directory_zip)
     to_dict = "static/images/download/" + str(hash_id)+".zip"
     data[0].update({"project_id_hash":to_dict})
-
+    db.close()
     return render_template('case.html', case=data)
 
 MEDIA_FOLDER_1 = os.path.join(APP_ROOT, 'static/images/output/')
